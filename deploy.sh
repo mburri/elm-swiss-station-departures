06579d550396280bcbe1ac1e962c25800bba8519
Max Burri <burri@puzzle.ch>
2020-04-17 23:00:24 +0200

    new deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+make prod
+git checkout gh-pages
+cp -r dist/* .
+git clean -f -x -d .
+git commit -am"deploy to gh-pages"
+git push
